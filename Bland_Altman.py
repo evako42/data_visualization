98d2e3bfc1d3cc71726ef7d23130fe6e065b8093
Eva Koderman <koderman.eva@gmail.com>
2024-02-17 19:40:41 +0100

    Add files via upload

---
+++ Bland_Altman.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.stats import shapiro
+
+# Load files
+file_path = 'excel_data_experiment.xlsx'
+control_condition = pd.read_excel(file_path, sheet_name='control_condition')
+experimental_condition = pd.read_excel(file_path, sheet_name='experimental_condition')
+
+def rand_jitter(arr):
+    stdev = .1 * (max(arr) - min(arr))
+    return arr + np.random.randn(len(arr)) * stdev
+
+def bland_altman_plot(data1, data2, plot_title:str):
+    data1 = np.asarray(data1)
+    data2 = np.asarray(data2)
+    mean = np.mean([data1, data2], axis=0)
+
+    diff = data1 - data2
+    #diff = rand_jitter(diff)
+
+    # Perform the Shapiro-Wilk test to check for normal distribution
+    _, p_value = shapiro(diff)
+    alpha = 0.05
+    # Check the p-value against the significance level
+    if p_value > alpha:
+        print(f"The diff data appears to be normally distributed (p-value: {p_value:.4f})")
+    else:
+        print(f"The diff data does not appear to be normally distributed (p-value: {p_value:.4f})")
+
+    md = np.mean(diff)                   # Mean of the difference
+    sd = np.std(diff, axis=0)            # Standard deviation of the difference
+    positive_lima = md + 1.96*sd         # Limits of agreement
+    negative_lima = md - 1.96*sd         # Limits of agreement
+
+    plt.scatter(mean, diff)
+    plt.axhline(md, color='black', linestyle='-', label= f'Mean of the difference: {round(md,3)}')
+    plt.axhline(positive_lima, color='gray', linestyle='--', label = f'Mean + 95% 2SD: {round(positive_lima, 2)}')
+    plt.axhline(negative_lima, color='gray', linestyle='--', label = f'Mean - 95% 2SD: {round(negative_lima, 2)}')
+    plt.ylim(-40,30)
+    plt.legend()
+    plt.xlabel('Mean')
+    plt.ylabel(f'Difference')
+    plt.title(f'{plot_title}')
+    plt.show()
+
+bland_altman_plot(control_condition, experimental_condition, 'Condition comparison with BA plot')
---
+++ linear_regression_plot.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from scipy.stats import spearmanr
+
+file_path = 'experiment_data.xlsx'
+experimental_condition = pd.read_excel(file_path, sheet_name='data1')
+control_condition = pd.read_excel(file_path, sheet_name='data2')
+
+def make_lr_plot(x_val:str, y_val:str, dataf:object, jitter:float, plot_title:str):
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+
+        # Linear regression model
+        lm = sns.lmplot(x=x_val,y=y_val, data=dataf,x_jitter=jitter)
+        ax = lm.axes
+        ax = ax[0,0]
+        plt.ylim(-10,80)
+        plt.xlim(-10,80)
+
+        # The diagonal line
+        ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", color='gray')
+        plt.xlabel(f"{x_val}", size=18)
+        plt.ylabel(f"{y_val}",size=18)
+
+        # Get the stats
+        from scipy.stats import linregress
+        lr = linregress(dataf[x_val], dataf[y_val])
+        r_value = lr[2]
+        r_squared = r_value**2
+
+        # Use spearmanr to calculate the Spearman's rank correlation
+        correlation, p_value = spearmanr(dataf[x_val], dataf[y_val])
+
+        # Print the result
+        print(f"Spearman's correlation: {round(correlation, 4)}")
+        print(f"P-value: {round(p_value,10)}")
+
+        ax = plt.suptitle(f'{plot_title}\nR2: {round(r_squared,3)}',size=15, style='oblique')
+        plt.show()
+
+# Merge the DataFrames based on the 'Subject' column
+merged_df = pd.merge(experimental_condition, control_condition, on='Subjects')
+merged_df = merged_df.drop_duplicates(subset='Subjects')
+
+for column in merged_df.columns:
+    if column != 'Subjects':
+        merged_df[column] = pd.to_numeric(merged_df[column], errors='coerce')
+
+#print(merged_df.dtypes)
+print(merged_df)
+condition = 5
+make_lr_plot(x_val= f'{condition}_x', y_val = f'{condition}_y', dataf = merged_df , jitter = 0.5, plot_title = 'Experiment 1')
